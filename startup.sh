452f785a4b420c29b367710affef1acf6a312ee7
ilhameouhaddou-ctrl <ilhameouhaddou@gmail.com>
2026-07-22 17:16:48 +0200

    Refactor: simplifier le script de démarrage pour NiceGUI en supprimant les variables d'environnement inutiles

--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-# NiceGUI lit PORT depuis l'env (défini par Azure) et bind sur 0.0.0.0 côté script.
-export PORT="${PORT:-8000}"
-python mail_manager/nicegui_app.py
+python mail_manager/nicegui_app.py \
+    --server.port=8000 \
+    --server.address=0.0.0.0
